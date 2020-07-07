a2843adafb5f4ad63a71db5b3a0018f1234f4f3e
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-07-07 09:25:05 -0700

    Change delete, post status code and test

--- light/data_model/tests/test_tornado_server.py
+++ light/data_model/tests/test_tornado_server.py
@@ -106,7 +106,7 @@ class TestWorldSaving(AsyncHTTPTestCase):
         
 
         response = yield self.client.fetch(
-            f'{URL}/builder/world/delete/{world1}?player={player_id}', method='POST', body=b''
+            f'{URL}/builder/world/delete/{world1}?player={player_id}', method='DELETE', body=b''
         )
         self.assertEqual(response.code, 200)
         self.assertEqual(json.loads(response.body.decode()),  str(world1))
@@ -120,7 +120,7 @@ class TestWorldSaving(AsyncHTTPTestCase):
         response = yield self.client.fetch(
             f'{URL}/builder/world/?player={player_id}', method='POST', headers=headers, body=b''
         )
-        self.assertEqual(response.code, 200)
+        self.assertEqual(response.code, 201)
         # Get back a world code, 
         self.assertEqual(type(json.loads(response.body.decode())), int)
     
@@ -199,7 +199,7 @@ class TestWorldSaving(AsyncHTTPTestCase):
         response = yield self.client.fetch(
             f'{URL}/builder/world/?player={player_id}', method='POST', body=self.get_encoded_url_params(d)
         )
-        self.assertEqual(response.code, 200)
+        self.assertEqual(response.code, 201)
         self.assertEqual(type(json.loads(response.body.decode())), int)
         w_id = json.loads(response.body.decode())
 
@@ -230,7 +230,7 @@ class TestWorldSaving(AsyncHTTPTestCase):
 
         # Test deletion
         response = yield self.client.fetch(
-            f'{URL}/builder/world/delete/{w_id}?player={player_id}', method='POST', body=b''
+            f'{URL}/builder/world/delete/{w_id}?player={player_id}', method='DELETE', body=b''
         )
         self.assertEqual(response.code, 200)
         self.assertEqual(json.loads(response.body.decode()),  str(w_id))
