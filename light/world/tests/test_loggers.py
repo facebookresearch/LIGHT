b95d82a0fe9478f1c5829b18701750accee66ce0
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-07-15 14:16:45 -0700

    Added unit test for all but observe_events

--- light/world/tests/test_loggers.py
+++ light/world/tests/test_loggers.py
@@ -14,16 +14,281 @@ from light.graph.structured_graph import OOGraph
 from light.world.world import World
 from light.graph.events.graph_events import ArriveEvent, LeaveEvent, GoEvent, LookEvent
 from light.world.content_loggers import AgentInteractionLogger, RoomInteractionLogger
+from parlai.core.params import ParlaiParser
 
 class TestInteractionLoggers(unittest.TestCase):
     """Unit tests for Interaction Loggers"""
 
     def setUp(self):
         self.data_dir = tempfile.mkdtemp()
+        self.parser = ParlaiParser()
+        self.parser.add_argument(
+            '--is-logging',
+            type='bool',
+            default=True,
+            help="Log events with interaction loggers",
+        )
+        self.parser.add_argument(
+            '--log-path',
+            type=str,
+            default=self.data_dir,
+            help="Write the events logged to this path",
+        )
 
     def tearDown(self):
         shutil.rmtree(self.data_dir)
+    
+    def setUp_single_room_graph(self):
+        # Set up the graph 
+        opt, _ = self.parser.parse_and_process_known_args()
+        test_graph = OOGraph(opt)
+        agent_node = test_graph.add_agent("My test agent", {})
+        room_node = test_graph.add_room("test room", {})
+        agent_node.force_move_to(room_node)
+        test_world = World({}, None, True)
+        test_world.oo_graph = test_graph 
+        return (test_graph, test_world, agent_node, room_node)
 
+    # Test individual methods first - 2 init tests
+    def test_init_room_logger(self):
+        """
+        Test the initial state of room loggers from OOGraph() constructor matches expected values
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+
+        self.assertEqual(logger.data_path, self.data_dir)
+        self.assertEqual(logger.graph, test_graph)
+        self.assertEqual(logger.state_history, [])
+        self.assertEqual(logger.event_buffer, [])
+        self.assertEqual(logger.room_id, room_node.node_id)
+        self.assertFalse(logger._is_logging())
+        self.assertTrue(logger._is_players_afk())
+        self.assertTrue(logger.is_active)
+        self.assertEqual(len(logger.context_buffer), 0)
+    
+    def test_init_agent_logger(self):
+        """
+        Test the initial state of the agent logger from OOGraph
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = AgentInteractionLogger(test_graph, agent_node)
+                
+        self.assertEqual(logger.data_path, self.data_dir)
+        self.assertEqual(logger.graph, test_graph)
+        self.assertEqual(logger.state_history, [])
+        self.assertEqual(logger.event_buffer, [])
+        self.assertEqual(logger.agent, agent_node)
+        self.assertFalse(logger._logging_intialized)
+        self.assertFalse(logger._is_player_afk())
+        self.assertTrue(logger.is_active)
+        self.assertEqual(len(logger.context_buffer), 0)
+
+    def test_begin_meta_episode_room_logger(self):
+        """
+        Test calling begin_meta_episode:
+            - Clears all the buffers (context into event if nonempty)
+            - adds the graph state from the room POV
+            - counts as a turn of player action
+            - initializes logger
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+        logger.event_buffer.append("Testing NOT!")
+        logger.state_history.append("Testing")
+        logger.context_buffer.append("Testing")
+        logger._begin_meta_episode()
+
+        self.assertFalse(logger._is_players_afk())
+        self.assertEqual(len(logger.context_buffer), 0)
+        self.assertEqual(logger.event_buffer, ["Testing"])
+        self.assertEqual(len(logger.state_history), 1)
+        self.assertEqual(
+            logger.state_history[-1], 
+            test_graph.to_json_rv(logger.room_id)
+        )
+
+    def test_begin_meta_episode_agent_logger(self):
+        """
+        Test calling begin_meta_episode:
+            - Clears all the buffers
+            - adds the graph state from the agent's room POV
+            - counts as a turn of player action
+            - initializes logger
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = AgentInteractionLogger(test_graph, agent_node)
+        logger.event_buffer.append("Testing")
+        logger.state_history.append("Testing")
+        logger._begin_meta_episode()
+
+
+        self.assertFalse(logger._is_player_afk())
+        self.assertTrue(logger._logging_intialized)
+        self.assertEqual(len(logger.context_buffer), 0)
+        self.assertEqual(len(logger.event_buffer), 0)
+        self.assertEqual(len(logger.state_history), 1)
+        self.assertEqual(
+            logger.state_history[-1], 
+            test_graph.to_json_rv(agent_node.get_room().node_id)
+        )
+
+    # Test end_meta_episode - 2 test
+    def test_end_meta_episode_room_logger(self):
+        """
+        Test calling end_meta_episode:
+            - Clears the context buffer
+            ** Note, future test check that things are written properly
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+        logger.context_buffer.append("Testing")
+        logger._end_meta_episode()
+        self.assertEqual(len(logger.context_buffer), 0)
+        
+    def test_end_meta_episode_agent_logger(self):
+        """
+        Test calling end_meta_episode:
+            - Makes the logging intialized false again
+            !! Note, will not have to worry about afk player ending (need the event)
+            so we will not have to clear context buffer
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = AgentInteractionLogger(test_graph, agent_node)
+        logger._end_meta_episode()
+        self.assertFalse(logger._logging_intialized)
+
+    # Test add and remove player for agent logger
+    def test_add_player_room_logger(self):
+        """
+        Test calling _add_player():
+            - Starts logging and adds a player if there is none
+            - just increments the count otherwise
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+        logger.event_buffer.append("Testing NOT!")
+        logger.state_history.append("Testing")
+        logger.context_buffer.append("Testing")
+        logger._add_player()
+
+        # The _loggging_active checks
+        self.assertTrue(logger._is_logging())
+        self.assertFalse(logger._is_players_afk())
+        self.assertEqual(len(logger.context_buffer), 0)
+        self.assertEqual(logger.event_buffer, ["Testing"])
+        self.assertEqual(len(logger.state_history), 1)
+        self.assertEqual(
+            logger.state_history[-1], 
+            test_graph.to_json_rv(logger.room_id)
+        )
+        self.assertEqual(logger.num_players_present, 1)
+
+        # Another player just ups the count
+        logger._add_player()
+        self.assertTrue(logger._is_logging())
+        self.assertFalse(logger._is_players_afk())
+        self.assertEqual(len(logger.context_buffer), 0)
+        self.assertEqual(logger.event_buffer, ["Testing"])
+        self.assertEqual(len(logger.state_history), 1)
+        self.assertEqual(
+            logger.state_history[-1], 
+            test_graph.to_json_rv(logger.room_id)
+        )
+        self.assertEqual(logger.num_players_present, 2)
+
+    def test_remove_player_room_logger(self):
+        """
+        Test calling _remove_player():
+            - just decrements the count if multiple players
+            - ends episode (meta_episode) otherwise
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+        logger._add_player()
+        logger._add_player()
+
+        logger._remove_player()
+        self.assertTrue(logger._is_logging())
+        self.assertFalse(logger._is_players_afk())
+        self.assertEqual(logger.num_players_present, 1)
+
+        # Another player is 0, end episode
+        logger._remove_player()
+        self.assertFalse(logger._is_logging())
+        self.assertEqual(len(logger.context_buffer), 0)
+        self.assertEqual(logger.num_players_present, 0)
+
+    # Test observe event - 2 test, 2 for afk, 1 for before room
+    def test_observer_event_goes_context_room_logger(self):
+        """
+        Test calling observe_event under normal circumstances adds the 
+        necessary metadata to the event buffer
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = test_graph.room_id_to_loggers[room_node.node_id]
+        test_event1 = ArriveEvent(agent_node, text_content="hello1")
+        test_event2 = ArriveEvent(agent_node, text_content="hello2")
+        test_event3 = ArriveEvent(agent_node, text_content="hello3")
+        test_event4 = ArriveEvent(agent_node, text_content="hello4")
+        test_event5 = ArriveEvent(agent_node, text_content="hello5")
+        test_event6 = ArriveEvent(agent_node, text_content="hello6")
+
+        # No player in room, so this should go to context
+        logger.observe_event(test_event1)
+        self.assertFalse(logger._is_logging())
+        self.assertEqual(len(logger.event_buffer), 0)
+        self.assertEqual(len(logger.context_buffer), 1)
+
+        logger.observe_event(test_event2)
+        logger.observe_event(test_event3)
+        logger.observe_event(test_event4)
+        logger.observe_event(test_event5)
+        logger.observe_event(test_event6)
+        self.assertFalse(logger._is_logging())
+        self.assertEqual(len(logger.event_buffer), 0)
+        self.assertEqual(len(logger.context_buffer), 5)
+        events = [json for _, _, _, json in logger.context_buffer]
+        self.assertFalse(test_event1.to_json() in events)
+
+    def test_observe_event_room_logger(self):
+        pass
+
+    def test_observe_event_agent_logger(self):
+        """
+        Test calling observe_event under normal circumstances adds the 
+        necessary metadata to the event buffer
+        """
+        initial = self.setUp_single_room_graph()
+        test_graph, test_world, agent_node, room_node = initial
+        # Not test player soul rn, so just copy what the method looks like
+        logger = AgentInteractionLogger(test_graph, agent_node)
+        logger._end_meta_episode
+        self.assertFalse(logger._logging_intialized)
+
+    def test_afk_observe_event_room_logger(self):
+        pass
+
+    def test_afk_observe_event_agent_logger(self):
+        pass
+    
     def test_simple_room_logger_saves_and_loads_init_graph(self):
         """
         Test that the room logger properly saves and reloads the initial 
@@ -139,7 +404,6 @@ class TestInteractionLoggers(unittest.TestCase):
                 parity += 1
                 parity %= 3
 
-    # TODO: Add simple unit type test - create new graph, loggers, log the events seperate tada!
     def test_simple_room_loggers_graph(self):
         """
         Test that the room logger properly saves and reloads the graph and events
@@ -216,12 +480,8 @@ class TestInteractionLoggers(unittest.TestCase):
         test_event = GoEvent(agent_node, target_nodes=[room_node2]).execute(test_world)
         # Need a way to make soul do w/e for testing
         pass
-                
-    # TODO: Add context unit test
 
-    # TODO: Add afk unit test
-
-    # TODO: Add e2e test - create a new starspace, ensure loggers log properly
+    # TODO: Add e2e test - create new graph, test room and agents
 
 if __name__ == "__main__":
     unittest.main()
