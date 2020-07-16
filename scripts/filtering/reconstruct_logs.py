901b72d94d3adc1839553079f84d8f08466e23bd
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-07-16 06:56:02 -0700

    Log script work better with new format

--- scripts/filtering/reconstruct_logs.py
+++ scripts/filtering/reconstruct_logs.py
@@ -12,24 +12,35 @@ the point of view (and logs) of either an agent or a room.
 # General flow: take given events file, go through, rebuilding the buffers
 # as appropriate. Should be super simple!
 
-def load_event(log_id):
+def load_event_log(event_file):
     '''
-    Given a log id, this method reconstructs the graph jsons and event jsons associated with said log.
-    To replay, all that is required is using the .from_json methods of the OOGraph and GraphEvent to
-    rebuild whatever events you want!
+    Given a log file, this method reconstructs the graph jsons and event jsons associated with said log.
+    To replay, all that is required is using event execute methods
     '''
-    event_file = os.path.join("/private/home/lucaskabela/LIGHT/logs/light_event_dumps/room",f'{log_id}_events.json')
-    buffer = []
+    graph_dir = os.
+    graph_buffer = []
+    event_buffer = []
     self.assertNotEqual(os.stat(event_file).st_size, 0)
     with open(event_file, 'r') as event_json_file:
-        parity = True
+        parity = 0
         for line in event_json_file:
-            if parity:
-                time_stamp = line
-            else:
-                written_event = line
-                buffer.append(written_event)
-            parity = not parity
+            if parity == 0:
+                line = line.strip()
+                self.assertEqual(room_logger._last_graph, line)
+            if parity == 2:
+                written_event = json.loads(line)
+                ref_json = json.loads(event_room_node_observed)
+                self.assertEqual(ref_json, written_event)
+            parity += 1
+            parity %= 3
+
+def main():
+    parser = argparse.ArgumentParser(description='Choose the event to reconstruct')
+    parser.add_argument('--event-file', type=str,
+                        help='Cookie secret for issueing cookies (SECRET!!!)')
+    FLAGS, _unknown = parser.parse_known_args()
+
+    load_event_log(FLAGS.event_file)
 
 if __name__ == '__main__':
-    load_event(0)
+    main()
