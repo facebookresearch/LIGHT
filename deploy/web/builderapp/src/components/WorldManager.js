4fb9ffe5933813483a05ecbc1a27f00cb5bfe041
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-07-24 09:43:42 -0700

    Prettier manager

--- deploy/web/builderapp/src/components/WorldManager.js
+++ deploy/web/builderapp/src/components/WorldManager.js
@@ -43,15 +43,21 @@ function ListWorlds({ isOpen, setIsOverlayOpen }) {
   const [upload, setUpload] = React.useState(undefined);
 
   const deleteWorld = async (id) => {
-    const res = await fetch(`${CONFIG.host}:${CONFIG.port}/builder/world/delete/${id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `${CONFIG.host}:${CONFIG.port}/builder/world/delete/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
   };
 
   const getAutosave = async () => {
-    const res = await fetch(`${CONFIG.host}:${CONFIG.port}/builder/world/autosave/`, {
-      method: "GET",
-    });
+    const res = await fetch(
+      `${CONFIG.host}:${CONFIG.port}/builder/world/autosave/`,
+      {
+        method: "GET",
+      }
+    );
     const data = await res.json();
 
     // Mission accomplished!
@@ -60,7 +66,7 @@ function ListWorlds({ isOpen, setIsOverlayOpen }) {
       message: "Done loading!",
     });
     setUpload(data);
-  }
+  };
 
   /* Given an entity id, its type, and the local entities store, get 
         the matching emoji and associate the type with the id */
@@ -125,18 +131,18 @@ function ListWorlds({ isOpen, setIsOverlayOpen }) {
       `${tileInfo.x} ${tileInfo.y + 1}`,
     ];
     let myLoc = `${tileInfo.x} ${tileInfo.y}`;
-    for(let i = 0; i < tileInfo.walls.length; i++){
-      if (tileInfo.walls[i]){
+    for (let i = 0; i < tileInfo.walls.length; i++) {
+      if (tileInfo.walls[i]) {
         let otherLoc = neighbors[i];
         // Need to sort in order for upload to work!
-        if (otherLoc < myLoc){
+        if (otherLoc < myLoc) {
           datMap[tileInfo.floor].walls[otherLoc + "|" + myLoc] = true;
-        }else{
+        } else {
           datMap[tileInfo.floor].walls[myLoc + "|" + otherLoc] = true;
         }
       }
     }
-  }
+  };
 
   /* Given the metadata for tiles, construct the format expected by the frontend which
         involves mapping x, y, and floor into the map then storing the other data */
@@ -228,26 +234,32 @@ function ListWorlds({ isOpen, setIsOverlayOpen }) {
   } else {
     const data = result.data;
     var auto = null;
-    if (result.auto == null){
+    if (result.auto == null) {
       auto = <></>;
-    }else{
-      auto = <><tr>
-      <td><strong>Autosave</strong></td>
-      <td> {result.auto} </td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td>                   
-        <Button
-              intent={Intent.PRIMARY}
-              type="submit"
-              onClick={() => getAutosave()}
-            >
-              Load
-        </Button>
-      </td>
-      <td></td>
-    </tr></>;
+    } else {
+      auto = (
+        <>
+          <tr>
+            <td>
+              <strong>Autosave</strong>
+            </td>
+            <td> {result.auto} </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <Button
+                intent={Intent.PRIMARY}
+                type="submit"
+                onClick={() => getAutosave()}
+              >
+                Load
+              </Button>
+            </td>
+            <td></td>
+          </tr>
+        </>
+      );
     }
     return (
       <>
@@ -324,11 +336,11 @@ function ListWorlds({ isOpen, setIsOverlayOpen }) {
   3. Surface the url param/link on the page
       * might be judicious to impose some limit on number of launches a user gets per session
 */
-export async function launchWorld(state){
+export async function launchWorld(state) {
   // TODO: See above
   const world_id = await postWorld(state);
-  const world_map = {'world_id': world_id}
-  const res = await post('game/new/', world_map)
+  const world_map = { world_id: world_id };
+  const res = await post("game/new/", world_map);
   const data = await res.json();
   const url = `${CONFIG.host}:${CONFIG.port}/?id=${data}`;
   window.open(url);
@@ -338,8 +350,8 @@ export async function launchWorld(state){
 export async function postAutosave(state) {
   // Just send the state dict
   const data = cloneDeep(state);
-  state = {data: data}
-  const res = await post("world/autosave/", state);
+  state = { data: data };
+  const res = await post("builder/world/autosave/", state);
 }
 
 export async function postWorld(state) {
@@ -349,7 +361,7 @@ export async function postWorld(state) {
     map: { tiles: [], edges: [] },
     entities: cloneDeep(state.entities),
   };
-  if (!("id" in dat.dimensions)){
+  if (!("id" in dat.dimensions)) {
     dat.dimensions["id"] = null;
   }
   const map = state.filteredMap();
