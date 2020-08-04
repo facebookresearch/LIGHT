23e4833f6f07c48809f3a5c6f62af8f53775ac19
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-08-04 11:28:40 -0700

    Skeleton for writing the episodes

--- scripts/filtering/construct_dataset.py
+++ scripts/filtering/construct_dataset.py
@@ -17,6 +17,8 @@
 from scripts.filtering.reconstruct_logs import load_event_log
 from scripts.filtering.extract_episodes import extract_episodes
 import argparse
+import os
+import uuid
 
 
 def convert_event_log(event_file, dataset_dir):
@@ -33,11 +35,21 @@ def write_episodes_to_dir(episodes, dataset_dir):
     """
         Given episodes and a directory for a dataset, write the episodes to the dataset
     """
+    if not os.path.exists(dataset_dir):
+        os.mkdir(dataset_dir)
+
+    # Decide what to do with the naming (?)
     for episode in episodes:
         for utter in episode.convo:
-            print(utter.actor_id, "said")
-            print(utter.text)
-    pass
+            unique_name = str(uuid.uuid4())
+            # What format!?!?!?
+            file_name = f"{unique_name}.csv"
+            file_path = os.path.join(dataset_dir, file_name)
+            # Decide format - all episodes in one file?  Many files?
+            # How to structure?
+            with open(file_path, "w") as episode_file:
+                # Fill in what to write properly
+                episode_file.write(utter)
 
 
 def main():
