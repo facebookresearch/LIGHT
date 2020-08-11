3fdc224cdf9e7c184ac41a48573aaf76324a12d2
Lucas Kabela <lucaskabela@devfair0178.h2.fair>
2020-08-11 13:55:22 -0700

    Some cleanup on triggered logic, graph events

--- light/graph/events/graph_events.py
+++ light/graph/events/graph_events.py
@@ -419,8 +419,6 @@ class GoEvent(GraphEvent):
         # Must store room views because getting views from other rooms is odd
         new_room = self.target_nodes[0]
         old_room = self.actor.get_room()
-        old_room_view = self.room.get_prefix_view_from(new_room)
-        # Need to make an exception for logging go events here, otherwise they never get logged
         self.__canonical_room_view = new_room.get_view_from(old_room)
 
     def execute(self, world: "World") -> List[GraphEvent]:
@@ -434,11 +432,12 @@ class GoEvent(GraphEvent):
         new_room = self.target_nodes[0]
         old_room_view = old_room.get_prefix_view_from(new_room)
 
-        self.__canonical_room_view = new_room.get_view_from(old_room)
+        # Send event to loggers - this event does not get broadcasted, so need record
         world.oo_graph.room_id_to_loggers[old_room.node_id].observe_event(self)
         world.purgatory.node_id_to_soul[self.actor.node_id].agent_logger.observe_event(
             self
         )
+
         # Trigger the leave event, must be before the move to get correct room
         LeaveEvent(self.actor, [self.target_nodes[0]]).execute(world)
         self.actor.move_to(new_room)
