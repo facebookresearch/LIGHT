2348bad69f27613e7be79d91b0bb94885d621724
Jack Urbanek <jju@fb.com>
2020-11-02 16:19:45 -0500

    Configuring for specific deploy from command line

--- deploy/web/build.sh
+++ deploy/web/build.sh
@@ -6,6 +6,19 @@ BUILDER_DIR="${WEBDIR}/builderapp/"
 GAME_DIR="${WEBDIR}/gameapp/"
 SERVER_FILE="${WEBDIR}/server/run_server.py"
 
+CONF_FN="./configs/"$1"/config.js"
+
+if [ -z "$1" ];
+then
+  echo "Must provide a config name from configs as an argument"
+  cd "configs"
+  ls
+  exit 1
+fi
+
+cp $CONF_FN $BUILDER_DIR"src/config.js"
+cp $CONF_FN $GAME_DIR"src/config.js"
+
 mkdir build
 cd $BUILDER_DIR
 npm run build
