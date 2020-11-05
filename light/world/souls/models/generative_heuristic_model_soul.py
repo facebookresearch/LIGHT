1ccbaf7d35325cc34e2992ece0c98e90f0fdf569
Jack Urbanek <jju@fb.com>
2020-11-05 13:55:34 -0500

    Removing copypasta from generative heuristic

--- light/world/souls/models/generative_heuristic_model_soul.py
+++ light/world/souls/models/generative_heuristic_model_soul.py
@@ -4,38 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import time
-import random
-from collections import deque, defaultdict
 from light.world.souls.models.partner_heuristic_model_soul import PartnerHeuristicModelSoul
-from light.graph.events.base import ErrorEvent
-from light.graph.events.graph_events import TellEvent, SayEvent
-from parlai.core.agents import create_agent_from_shared
-
-from typing import TYPE_CHECKING, List
-
-if TYPE_CHECKING:
-    from light.graph.elements.graph_nodes import GraphAgent
-    from light.graph.world.world import World
-    from light.graph.events.base import GraphEvent
-
-
-MIN_TIME_BETWEEN_TURNS = 5
-MAX_DIALOGUE_REPEAT_HISTORY = 50
-MAX_ACTION_REPEAT_HISTORY = 4
-ALLOW_INTERBOT_CHAT = False  # Only allow bots to answer humans
-JITTER_TIME_AROUND_TURNS = 2
-CHAT_DISENGAGE_CHANCE = 5.0 / 100.0
-TAKE_ACTION_CHANCE = 1.0 / 5.0
-SAFE_PHRASES = [
-  "Let's talk about something else?",
-  "Tell me something you don't know anything about.",
-  "I'd like to talk about something different.",
-  "Would you like to talk about something different.",
-  "Can we change topics?",
-  "How about a new conversation?",
-]
-
+from parlai.core.agents import create_agent
 
 class GenerativeHeuristicModelSoul(PartnerHeuristicModelSoul):
     """
