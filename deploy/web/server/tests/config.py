3f37e772610157a54607cab485e5cbc8bd4f8a57
Jack Urbanek <jju@fb.com>
2020-12-18 17:02:46 +0000

    forgot some imports

--- deploy/web/server/tests/config.py
+++ deploy/web/server/tests/config.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+import os.path
+import inspect
+
+def get_path(filename):
+    cwd = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return os.path.join(cwd, filename)
+
 TEST_TORNADO_SETTINGS = {
     "autoescape": None,
     "cookie_secret": "0123456789",
