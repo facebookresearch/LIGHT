acc8660ce7ff0519a774649b25a51f02390fdf35
ericpqmor <ericpqueiroz@fb.com>
2021-04-07 10:43:47 -0700

    Fixing validation on task 1

--- crowdsourcing/custom_world_interactions/run_task.py
+++ crowdsourcing/custom_world_interactions/run_task.py
@@ -18,6 +18,10 @@ from mephisto.abstractions.blueprints.abstract.static_task.static_blueprint impo
     SharedStaticTaskState,
 )
 from light.data_model.light_database import LIGHTDatabase
+from mephisto.abstractions.databases.local_database import LocalMephistoDB
+from mephisto.tools.data_browser import DataBrowser as MephistoDataBrowser
+from mephisto.data_model.worker import Worker
+from mephisto.data_model.unit import Unit
 
 import hydra
 import json
@@ -30,7 +34,9 @@ LIGHT_DB_PATH = "/checkpoint/light/data/database3.db"
 PRIMARY_OBJECT_LIST_SIZE = 5
 SECONDARY_OBJECT_LIST_SIZE = 5
 DEFAULT_NUM_TASKS = 20
-BLOCK_QUALIFICATION = "The answer should be a full phrase starting with 'You...'"
+
+db = LocalMephistoDB()
+mephisto_data_browser = MephistoDataBrowser(db=db)
 
 defaults = [
     {"mephisto/blueprint": BLUEPRINT_TYPE},
@@ -49,7 +55,6 @@ class TestScriptConfig(RunScriptConfig):
     primary_object_list_size: int = PRIMARY_OBJECT_LIST_SIZE
     secondary_object_list_size: int = SECONDARY_OBJECT_LIST_SIZE
     num_tasks: int = DEFAULT_NUM_TASKS
-    block_qualification: str = BLOCK_QUALIFICATION
 
 def get_object_list(db_path):
     db = LIGHTDatabase(db_path)
@@ -102,19 +107,18 @@ def build_task(task_dir):
     os.chdir(return_dir)
 
 def validate_unit(unit):
-    print("Inside validation function")
-
     if unit.get_assigned_agent() is None:
         return
 
-    data = unit.get_assigned_agent().get_data()
+    data = mephisto_data_browser.get_data_from_unit(unit)["data"]["outputs"]["final_data"]
+    action_description = data["actionDescription"]
 
-    if len(data) <= 20 or data.lower().find("you") == -1 or data.lower()[-1] != ".":
-        # Not in second person
-        print("Not validated unit!")
+    if len(action_description) <= 20 or action_description.lower().find("you") == -1 or (action_description.lower()[-1] != "." and action_description.lower()[-1] != "?" and action_description.lower()[-1] != "!"):
+        # Not in second person or invalid punctuation
+        print("Action " + action_description + " was not validated!")
         unit.get_assigned_agent().soft_reject_work()
         worker = unit.get_assigned_agent().get_worker()
-        worker.grant_qualification(cfg.block_qualification, 1)
+        worker.grant_qualification("objects_interaction_task_block", 1)
     return
 
 @hydra.main(config_name="scriptconfig")
