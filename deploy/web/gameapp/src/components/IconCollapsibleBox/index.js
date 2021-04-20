cfd076c8fa1cfbddf1f450eee4c65eb8c61bc8da
Justin Pinero <jpinero@fb.com>
2021-04-20 08:36:08 -0400

    added collapsible box with icon, fixing emoji picker

--- deploy/web/gameapp/src/components/IconCollapsibleBox/index.js
+++ deploy/web/gameapp/src/components/IconCollapsibleBox/index.js
@@ -25,66 +25,68 @@ const IconCollapsibleBox = ({
   const closeHandler = () => setIsCollapsed(true);
 
   return (
-    <div className="collapsible-container__icon">
-      <div
-        className={cx("icon", { editing: showEmojiPicker })}
-        style={{ cursor: "pointer" }}
-      >
-        <div className="overlay">edit</div>
-        <span
-          role="img"
-          aria-label="avatar"
-          onClick={() => setShowEmojiPicker(true)}
-        >
-          {selectedEmoji}
-        </span>
-        {showEmojiPicker ? (
-          <div
-            style={{
-              position: "absolute",
-              top: "80px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              zIndex: 999,
-            }}
-          >
-            <BlurClosingPicker
-              autoFocus={true}
-              onBlur={() => setShowEmojiPicker(false)}
-              onSelect={(emoji) => {
-                // TODO: Send the selected emoji to the back-end so we can keep record it
-                setSelectedEmoji(emoji.native);
-                setShowEmojiPicker(false);
-              }}
-            />
-          </div>
-        ) : null}
-      </div>
-      <div
-        className="collapsible-header"
-        style={{
-          backgroundColor: titleBg,
-          borderRadius: isCollapsed ? "15px" : null,
-        }}
-      >
-        <div />
-        <div className="collapsible-header--icon">
-          {isCollapsed ? (
-            <BiWindow style={{}} onClick={openHandler} />
-          ) : (
-            <FaWindowMinimize onClick={closeHandler} />
-          )}
-        </div>
-      </div>
-      <h5 className="collapsible-header--text">{title}</h5>
-      {isCollapsed ? null : (
+    <div style={{ marginTop: "70px" }}>
+      <div className="collapsible-container__icon">
         <div
-          className="collapsible-body"
-          style={{ backgroundColor: containerBg }}
+          className={cx("icon", { editing: showEmojiPicker })}
+          style={{ cursor: "pointer" }}
         >
-          {children}
+          <div className="overlay">edit</div>
+          <span
+            role="img"
+            aria-label="avatar"
+            onClick={() => setShowEmojiPicker(true)}
+          >
+            {selectedEmoji}
+          </span>
+          {showEmojiPicker ? (
+            <div
+              style={{
+                position: "absolute",
+                top: "80px",
+                left: "50%",
+                transform: "translateX(-50%)",
+                zIndex: 999,
+              }}
+            >
+              <BlurClosingPicker
+                autoFocus={true}
+                onBlur={() => setShowEmojiPicker(false)}
+                onSelect={(emoji) => {
+                  // TODO: Send the selected emoji to the back-end so we can keep record it
+                  setSelectedEmoji(emoji.native);
+                  setShowEmojiPicker(false);
+                }}
+              />
+            </div>
+          ) : null}
         </div>
-      )}
+        <div
+          className="collapsible-header"
+          style={{
+            backgroundColor: titleBg,
+            borderRadius: isCollapsed ? "15px" : null,
+          }}
+        >
+          <div />
+          <div className="collapsible-header--icon">
+            {isCollapsed ? (
+              <BiWindow style={{}} onClick={openHandler} />
+            ) : (
+              <FaWindowMinimize onClick={closeHandler} />
+            )}
+          </div>
+        </div>
+        <h5 className="collapsible-header--text">{title}</h5>
+        {isCollapsed ? null : (
+          <div
+            className="collapsible-body"
+            style={{ backgroundColor: containerBg }}
+          >
+            {children}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- deploy/web/gameapp/src/pages/GamePage/Sidebar/index.js
+++ deploy/web/gameapp/src/pages/GamePage/Sidebar/index.js
@@ -19,7 +19,7 @@ import { FaWindowMinimize } from "react-icons/fa";
 
 const EmojiPicker = ({ onBlur, ...props }) => {
   EmojiPicker.handleClickOutside = () => onBlur();
-  return <Picker {...props} />;
+  return <Picker style={{ zIndex: "100" }} {...props} />;
 };
 const BlurClosingPicker = onClickOutside(EmojiPicker, {
   handleClickOutside: () => EmojiPicker.handleClickOutside,
@@ -33,95 +33,27 @@ const SideBar = ({ persona, location, dataModelHost, getEntityId }) => {
 
   return (
     <div>
-      {persona ? (
-        <div className="persona">
-          <div
-            className={cx("icon", { editing: showEmojiPicker })}
-            style={{ cursor: "pointer" }}
+      <div
+        className={cx("icon", { editing: showEmojiPicker })}
+        style={{ cursor: "pointer" }}
+      >
+        {dataModelHost && (
+          <Tooltip
+            style={{ position: "absolute", bottom: 0, right: 5 }}
+            title={`suggest changes for ${persona.name}`}
+            position="bottom"
           >
-            <div className="overlay">edit</div>
-            <span
-              className="char-icon"
-              role="img"
-              aria-label="avatar"
-              onClick={() => setShowEmojiPicker(true)}
+            <a
+              className="data-model-deep-link"
+              href={`${dataModelHost}/edit/${getEntityId(persona.id)}`}
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              {selectedEmoji}
-            </span>
-            {showEmojiPicker ? (
-              <div
-                style={{
-                  position: "absolute",
-                  top: "80px",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                  zIndex: 999,
-                }}
-              >
-                <BlurClosingPicker
-                  autoFocus={true}
-                  onBlur={() => setShowEmojiPicker(false)}
-                  onSelect={(emoji) => {
-                    // TODO: Send the selected emoji to the back-end so we can keep record it
-                    setSelectedEmoji(emoji.native);
-                    setShowEmojiPicker(false);
-                  }}
-                />
-              </div>
-            ) : null}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "center",
-              margin: "10px",
-            }}
-          >
-            {showCharacter ? (
-              <FaWindowMinimize onClick={() => setShowCharacter(false)} />
-            ) : (
-              <BiWindow onClick={() => setShowCharacter(true)} />
-            )}
-          </div>
-          <h3
-            style={{
-              fontFamily: "fantasy",
-              fontWeight: "900",
-              marginTop: "6px",
-              fontSize: "large",
-            }}
-          >
-            You are {persona.prefix} {persona.name}
-          </h3>
-          {showCharacter ? (
-            <p className="persona-text" style={{ fontSize: "14px" }}>
-              {persona.description.slice(
-                0,
-                persona.description.indexOf("Your Mission:")
-              )}
-            </p>
-          ) : (
-            <div />
-          )}
-          {dataModelHost && (
-            <Tooltip
-              style={{ position: "absolute", bottom: 0, right: 5 }}
-              title={`suggest changes for ${persona.name}`}
-              position="bottom"
-            >
-              <a
-                className="data-model-deep-link"
-                href={`${dataModelHost}/edit/${getEntityId(persona.id)}`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fa fa-edit" aria-hidden="true" />
-              </a>
-            </Tooltip>
-          )}
-        </div>
-      ) : null}
+              <i className="fa fa-edit" aria-hidden="true" />
+            </a>
+          </Tooltip>
+        )}
+      </div>
       <IconCollapsibleBox
         title={`You are ${persona.prefix} ${persona.name}`}
         showEmojiPicker={showEmojiPicker}
