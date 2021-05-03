9f384256271fa43f51cb6aa88d18bc902a0fcc39
Justin Pinero <jpinero@fb.com>
2021-05-03 16:23:35 -0400

    added images to character basics

--- deploy/web/landingapp/src/pages/TutorialPage/Topics/CharacterBasics.js
+++ deploy/web/landingapp/src/pages/TutorialPage/Topics/CharacterBasics.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import "../styles.css";
 
+//IMAGES
+import CharacterBasicsQuest from "../../../assets/images/screenshots/Tutorial/CharacterBasics/CharacterBasicsQuest.png";
+import CharacterBasicsInventory from "../../../assets/images/screenshots/Tutorial/CharacterBasics/CharacterBasicsInventory.png";
+import CharacterBasicsStats from "../../../assets/images/screenshots/Tutorial/CharacterBasics/CharacterBasicsStats.png";
+
 const CharacterBasics = (props) => {
   return (
     <div className="characterbasics-container">
@@ -10,11 +15,14 @@ const CharacterBasics = (props) => {
         Quest: If you want to be reminded what character you are playing and
         your goals type “quest”:
       </p>
+      <img src={CharacterBasicsQuest} />
       <p>
         Inventory: To see what you are carrying you can type “inventory” or
         “inv” or even “i” for short:
       </p>
+      <img src={CharacterBasicsInventory} />
       <p>Stats: To see your stats, type “stats:</p>
+      <img src={CharacterBasicsStats} />
       <p>
         Help! You can also type “help” at any time to see a short list of
         instructions.
