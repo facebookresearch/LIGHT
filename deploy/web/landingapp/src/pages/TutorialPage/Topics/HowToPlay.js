f87d417b88904e0fa41eb6dac42fe4b57711c269
Justin Pinero <jpinero@fb.com>
2021-05-03 17:44:59 -0400

    resized tutorial header subheader and text classes

--- deploy/web/landingapp/src/pages/TutorialPage/Topics/HowToPlay.js
+++ deploy/web/landingapp/src/pages/TutorialPage/Topics/HowToPlay.js
@@ -5,19 +5,22 @@ import "../styles.css";
 const HowToPlay = (props) => {
   return (
     <div className="howtoplay-container">
-      <h1>How to Play LIGHT</h1>
-      <p>
+      <h1 className="tutorial-header">How to Play LIGHT</h1>
+      <p className="tutorial-text">
         You will be teleported into our mystical realm and fill the shoes of a
         character who lives there — playing that role you must act and talk with
         other LIGHT denizens. As you interact, your messages will be evaluated
         by the Dungeon Master AI. Portray your character well and you will
-        increase your *experience points*. Other characters will also be playing
-        their roles as well — some of them will be other human souls, others
-        will be AIs. When everyone is playing their role to the best of their
-        ability in the realm the experience will be maximized. You can *reward*
+        increase your{" "}
+        <span style={{ fontWeight: "bolder" }}>experience points</span>. Other
+        characters will also be playing their roles as well — some of them will
+        be other human souls, others will be AIs. When everyone is playing their
+        role to the best of their ability in the realm the experience will be
+        maximized. You can <span style={{ fontWeight: "bolder" }}>reward </span>
         other players when you are impressed by their skills. You can also
-        *report* or demote unwanted behaviors: e.g., mentions of the real world
-        or bad behavior. Stay good, my denizens!
+        <span style={{ fontWeight: "bolder" }}> report</span> or demote unwanted
+        behaviors: e.g., mentions of the real world or bad behavior. Stay good,
+        my denizens!
       </p>
     </div>
   );
