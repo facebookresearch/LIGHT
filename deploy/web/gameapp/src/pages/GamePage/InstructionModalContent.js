dcfa168969b971eb267d4da80e49b628d87c8093
Justin Pinero <jpinero@fb.com>
2021-05-04 15:35:43 -0400

    added copy to modal and back functionality to tutorial page back button

--- deploy/web/gameapp/src/pages/GamePage/InstructionModalContent.js
+++ deploy/web/gameapp/src/pages/GamePage/InstructionModalContent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-dom";
 
 import { BiWindow } from "react-icons/bi";
 import { FaWindowMinimize } from "react-icons/fa";
@@ -13,8 +14,23 @@ const InstructionModalContent = (props) => {
       <div className="instructionmodalcontent-header">
         <img className="instructionmodalcontent-image" src={Scribe} />
         <h1>
-          Welcome to <span>LIGHT</span>
+          Playing <span>LIGHT</span>
         </h1>
+      </div>
+      <div className="instructionmodalcontent-body">
+        <p className="instructionmodalcontent-body__text">
+          The LIGHT interface attempts to interpret anything typed into it as an
+          action.
+        </p>
+        <p className="instructionmodalcontent-body__text">
+          To speak, you should wrap your sentence in quotes, or use shift+enter
+          to send your messages.
+        </p>
+        <p className="instructionmodalcontent-body__text">
+          A list of commands can be displayed with "help" and you can find a
+          more complete list of instructions
+          <a href={"/#/tutorial"}>here </a>
+        </p>
       </div>
     </div>
   );
