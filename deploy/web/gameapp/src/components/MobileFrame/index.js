80efa4c291fe10c2082c2f6c000c299a277b61cd
Justin Pinero <jpinero@fb.com>
2021-05-13 13:09:13 -0400

    added toggleswitch, header, and buttons to mobile frame

---
+++ deploy/web/gameapp/src/components/MobileFrame/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+//CUSTOM COMPONENTS
+import MobileHeader from "./MobileHeader";
+
+const MobileFrame = ({
+  showDrawer,
+  openDrawer,
+  closeDrawer,
+  buttons,
+  children,
+}) => {
+  return (
+    <div className="mobile-container">
+      <MobileHeader
+        buttons={buttons}
+        showDrawer={showDrawer}
+        openDrawer={openDrawer}
+        closeDrawer={closerDrawer}
+      />
+      <div className="mobile-content">{children}</div>
+    </div>
+  );
+};
+
+export default MobileFrame;
