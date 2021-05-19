840910a341fd08e847a2c8fb8c179edc0b9341d1
Justin Pinero <jpinero@fb.com>
2021-05-19 19:41:35 -0400

    fixed alignment of example card content

--- crowdsourcing/custom_world_interactions/webapp/src/components/ExampleCard/index.js
+++ crowdsourcing/custom_world_interactions/webapp/src/components/ExampleCard/index.js
@@ -6,27 +6,54 @@ const ExampleCard =  ({primary, secondary, narration, badReason})=>(
     <div className="card-container">
         <div className="card-row">
             <div className="card-item">
-                <p className="card-text"><span className="card-label" style={{color:"gold"}}>PRIMARY:</span>{primary}</p>
-            </div>
-            <div className="card-item">
-                <p className="card-text"><span className="card-label" style={{color:"blue"}}>SECONDARY:</span>{secondary}</p>
-            </div>
-        </div>
-        <div className="card-row">
-            <div className="card-item">
-                <p className="card-text"><span className="card-label" style={{color:"orange"}}>ACTION:</span>{`USE ${primary} WITH ${secondary}`}</p>
+                <p className="card-item__label">
+                    <span style={{color:"gold"}}>PRIMARY:</span>
+                </p>
+                <p className="card-item__text">
+                    {primary}
+                </p>
             </div>
         </div>
         <div className="card-row">
             <div className="card-item">
-                <p className="card-text"><span className="card-label" style={{color:"green"}}>NARRATION:</span>{narration}</p>
+                <p className="card-item__label">
+                    <span style={{color:"blue"}}>SECONDARY:</span>
+                </p>
+                <p className="card-item__text">
+                    {secondary}
+                </p>
+            </div>
+        </div>
+        <div className="card-row">
+            <div className="card-item">
+                <p className="card-item__label">
+                    <span style={{color:"orange"}}>ACTION:</span>
+                </p>
+                <p className="card-item__text">
+                    {`USE ${primary} WITH ${secondary}`}
+                </p>
+            </div>
+        </div>
+        <div className="card-row">
+            <div className="card-item">
+                <p className="card-item__label">
+                    <span className="card-label" style={{color:"green"}}>NARRATION:</span>
+                </p>
+                <p className="card-item__text">
+                    {narration}
+                </p>
             </div>
         </div>
         {
         badReason ?
         <div className="card-row">
-            <div className="card-item">
-                <p className="card-text" style={{color:"red"}}><span className="card-label"><BsFillExclamationDiamondFill color="red" /> WHAT'S WRONG? </span> {badReason}</p>
+            <div className="card-item__badreasosn">
+                <p className="card-label__badreason" style={{color:"red"}}>
+                    <span><BsFillExclamationDiamondFill color="red" /> </span> WHAT'S WRONG?
+                </p>
+                <p className="card-item__text--badreason">
+                    {badReason}
+                </p>
             </div>
         </div>
         :
