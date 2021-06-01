0a696d0d22c2c0be7799994c9a974c9f6b2bf641
Justin Pinero <jpinero@fb.com>
2021-06-01 16:05:59 -0400

    building multiple choice

--- crowdsourcing/constraint_events/webapp/src/components/Questions/MultipleChoiceQuestion/index.js
+++ crowdsourcing/constraint_events/webapp/src/components/Questions/MultipleChoiceQuestion/index.js
@@ -1,12 +1,35 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
+import "./styles.css";
 
+//CUSTOM COMPONENTS
+import ObjectButton from "./ObjectButton";
 
-const MultipleChoiceQuestion = ({  }) => {
-    return (
-      <div>
-        MultipleChoiceQuestion
-      </div>
-    );
+const MultipleChoiceQuestion = ({question, answers})=>{
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [objectList, setObjectList] = useState([])
+
+    const clickHandler = (id, selection)=>{
+        setSelectedItem(id);
+        selectFunction(selection);
+    }
+    useEffect(()=>{
+        setObjectList(items)
+    }, [items])
+    return(
+        <div className="selector-container" >
+            <h1 className="selector-header">
+                {label}
+            </h1>
+            <div className="options-container">
+            {
+                [objectList].length
+                ?
+                objectList.map((item, index)=><ObjectButton key={index} name={item} selectFunction={()=>clickHandler(index, item)} isSelected={selectedItem==index} />)
+                :
+                null
+            }
+            </div>
+        </div>
+    )
 }
-
-export default MultipleChoiceQuestion ;
+export default MultipleChoiceQuestion;
