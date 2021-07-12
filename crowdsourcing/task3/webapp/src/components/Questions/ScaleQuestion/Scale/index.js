cd896c5cebc7d3afe98051012fb775f3c8036461
Justin Pinero <jpinero@fb.com>
2021-07-12 15:06:56 -0400

    added getDimensions utility function, added dimension state to scale component, connectect state to konva stage

--- crowdsourcing/task3/webapp/src/components/Questions/ScaleQuestion/Scale/index.js
+++ crowdsourcing/task3/webapp/src/components/Questions/ScaleQuestion/Scale/index.js
@@ -1,5 +1,5 @@
 //REACT
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 //KONVA
 import { Stage, Layer, Line} from 'react-konva';
 //STYLING
@@ -7,33 +7,43 @@ import "./styles.css"
 //CUSTOM COMPONENTS
 import ScaleRange from "./ScaleRange"
 import OptionBlock from "./OptionBlock"
-
+//Utils
+import GetWindowDimensions from "../../../../utils/GetDimensions.js"
+//Scale - Component that contains actual scale UI for ScaleQuestion component.  Allows user to drag and drop name plates on a range that will supply rating to the backend.
 const Scale = ({trait, actors}) => {
-
-  return (
-    <div className="scale-container">
-        <div style={{width:"100%", height:"50%" }}>
-            <div style={{width:"260px", backgroundColor:"white"}}>
-                <p style={{textAlign:"center", borderColor:"blue", borderWidth: "3px", borderStyle:"solid", padding:"2px", fontWeight:"bold", color:"blue"}}>
-                    ACTORS
-                </p>
-            </div>
-            <div >
-                <Stage width={window.innerWidth} height={window.innerHeight/2}>
-                    <Layer>
-                        <OptionBlock
-                            width={window.innerWidth}
-                            height={window.innerHeight/4}
-                            actors={actors}
-                        />
-                        <Line closed points={[100, 0, 100, -100, 0, 0]} fill="red" />
-                    </Layer>
-                </Stage>
-                <ScaleRange trait={trait} />
+    const [dimensions, setDimensions]= useState(GetWindowDimensions())
+    useEffect(()=>{
+        const handleResize = ()=>{
+            setDimensions(GetWindowDimensions())
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+    const {width, height}= dimensions
+    return (
+        <div className="scale-container">
+            <div style={{width:"100%", height:"50%" }}>
+                <div style={{width:"260px", backgroundColor:"white"}}>
+                    <p style={{textAlign:"center", borderColor:"blue", borderWidth: "3px", borderStyle:"solid", padding:"2px", fontWeight:"bold", color:"blue"}}>
+                        ACTORS
+                    </p>
+                </div>
+                <div >
+                    <Stage width={width} height={height/2}>
+                        <Layer>
+                            <OptionBlock
+                                width={width}
+                                height={height/4}
+                                actors={actors}
+                            />
+                            <Line closed points={[100, 0, 100, -100, 0, 0]} fill="red" />
+                        </Layer>
+                    </Stage>
+                    <ScaleRange trait={trait} />
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Scale;
