c1f2b94c75e3e06b6b811a10f2a6e675d792a5c1
Jack Urbanek <jju@fb.com>
2021-07-30 14:04:38 -0400

    Importing new examine script

--- crowdsourcing/quests/examine_results.py
+++ crowdsourcing/quests/examine_results.py
@@ -1,17 +1,41 @@
-from mephisto.abstractions.databases.local_database import LocalMephistoDB
-from mephisto.tools.data_browser import DataBrowser as MephistoDataBrowser
+from mephisto.core.local_database import LocalMephistoDB
+from mephisto.core.data_browser import DataBrowser as MephistoDataBrowser
 from mephisto.data_model.worker import Worker
+
 
 db = LocalMephistoDB()
 mephisto_data_browser = MephistoDataBrowser(db=db)
+
+DO_REVIEW = True
 
 units = mephisto_data_browser.get_units_for_task_name(input("Input task name: "))
 
 tasks_to_show = input("Tasks to see? (a)ll/(u)nreviewed: ")
 if tasks_to_show in ["all", "a"]:
-    pass
+    DO_REVIEW = False
 else:
+    others = [u for u in units if u.get_status() != "completed"]
     units = [u for u in units if u.get_status() == "completed"]
+    previous_work_by_worker = {}
+    for unit in others:
+        w_id = unit.worker_id
+        if w_id not in previous_work_by_worker:
+            previous_work_by_worker[w_id] = {
+                "accepted": [],
+                "soft_rejected": [],
+                "rejected": [],
+            }
+        previous_work_by_worker[w_id][unit.get_status()].append(unit)
+    print(
+        "You will be reviewing actual tasks with this flow. Tasks that you either Accept or Pass "
+        "will be paid out to the worker, while rejected tasks will not. Passed tasks will be "
+        "specially marked such that you can leave them out of your dataset. \n"
+        "When you pass on a task, the script gives you an option to disqualify the worker "
+        "from future tasks by assigning a qualification. If provided, this worker will no "
+        "longer be able to work on tasks where the set --block-qualification shares the same name.\n"
+        "You should only reject tasks when it is clear the worker has acted in bad faith, and "
+        "didn't actually do the task. Prefer to pass on tasks that were misunderstandings."
+    )
 
 
 def format_action(action_obj):
@@ -75,6 +99,64 @@ def format_for_printing_data(data):
     return f"-------------------\n{metadata_string}{inputs_string}{output_string}"
 
 
-for unit in units:
-    print(format_for_printing_data(mephisto_data_browser.get_data_from_unit(unit)))
-    # TODO can add review stuff if desired?
+def get_prev_stats(worker_id):
+    prev_work = previous_work_by_worker.get(worker_id)
+    if prev_work is None:
+        return "(First time worker!)"
+    accepted_work = len(prev_work["accepted"])
+    soft_rejected_work = len(prev_work["soft_rejected"])
+    rejected_work = len(prev_work["rejected"])
+    return f"({accepted_work} | {rejected_work + soft_rejected_work}({soft_rejected_work}) / {accepted_work + soft_rejected_work + rejected_work})"
+
+
+disqualification_name = "can-write-light-quests-live-failed"  # None
+
+units_by_worker = {}
+
+for u in units:
+    w_id = u.worker_id
+    if w_id not in units_by_worker:
+        units_by_worker[w_id] = []
+    units_by_worker[w_id].append(u)
+
+print(f"Need to review {len(units)} units by {len(units_by_worker)} different workers")
+
+for w_id, w_units in units_by_worker.items():
+    worker_name = Worker(db, w_id).worker_name
+    apply_all_keep = None
+    for idx, unit in enumerate(w_units):
+        print(
+            f"Reviewing for worker {worker_name}, ({idx+1}/{len(w_units)}), Previous {get_prev_stats(w_id)}"
+        )
+        print(format_for_printing_data(mephisto_data_browser.get_data_from_unit(unit)))
+        if DO_REVIEW:
+            if apply_all_keep is not None:
+                keep = apply_all_keep
+            else:
+                keep = input(
+                    "Do you want to accept this work? (a)ccept, (r)eject, (p)ass: "
+                )
+            if keep.lower() == "a":
+                unit.get_assigned_agent().approve_work()
+            elif keep.lower() == "r":
+                if apply_all_keep is None:
+                    reason = input("Why are you rejecting this work?")
+                unit.get_assigned_agent().reject_work(reason)
+            elif keep.lower() == "p":
+                # General best practice is to accept borderline work and then disqualify
+                # the worker from working on more of these tasks
+                agent = unit.get_assigned_agent()
+                agent.soft_reject_work()
+                if apply_all_keep is None:
+                    should_soft_block = input(
+                        "Do you want to soft block this worker? (y)es/(n)o: "
+                    )
+                    if should_soft_block.lower() in ["y", "yes"]:
+                        if disqualification_name == None:
+                            disqualification_name = input(
+                                "Please input the qualification name you are using to soft block for this task: "
+                            )
+                        worker = agent.get_worker()
+                        worker.grant_qualification(disqualification_name, 1)
+            if keep.lower() != keep:
+                apply_all_keep = keep
