a31efe0f599b8c3f5c6ce36884ebc3d2dad2e0fe
Justin Pinero <jpinero@fb.com>
2021-08-05 11:36:52 -0400

    added attribute questions component and added more documentation to Task component

---
+++ crowdsourcing/task3/webapp/src/views/Task/AttributeQuestions/index.js
@@ -0,0 +1,26 @@
+//REACT
+import React, {useState, useEffect} from "react";
+//STYLE
+import "./styles.css";
+//CUSTOM COMPONENTS
+
+
+
+// TagQuestion - Container for typeahead boolean values
+const AttributeQuestions = ({
+    header,//Text at head of container
+    selection,// Objects who's attributes are being added and removed
+    booleanAttributes, // the default attributes for the selected object type
+    updateFunction,// the function to update the attributes for the objects
+    numericAttributeUpdateFunction// tthe function that updates numeric attribute values
+})=>{
+
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default AttributeQuestions;
