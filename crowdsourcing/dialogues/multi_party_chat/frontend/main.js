f14ac2765be1dd9f12dec0c3025356549029d875
Jack Urbanek <jju@fb.com>
2021-09-09 13:59:47 -0400

    Multi-agent chat first pass

---
+++ crowdsourcing/dialogues/multi_party_chat/frontend/main.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap-chat/styles.css";
+
+import { ChatApp, ChatMessage, DefaultTaskDescription } from "bootstrap-chat";
+
+function RenderChatMessage({ message, mephistoContext, appContext, idx }) {
+  const { agentId } = mephistoContext;
+  const { currentAgentNames } = appContext.taskContext;
+
+  return (
+    <div>
+      <ChatMessage
+        isSelf={message.id === agentId || message.id in currentAgentNames}
+        agentName={
+          message.id in currentAgentNames
+            ? currentAgentNames[message.id]
+            : message.id
+        }
+        message={message.text}
+        taskData={message.task_data}
+        messageId={message.message_id}
+      />
+    </div>
+  );
+}
+
+function MainApp() {
+  return (
+    <ChatApp
+      renderMessage={({ message, idx, mephistoContext, appContext }) => (
+        <RenderChatMessage
+          message={message}
+          mephistoContext={mephistoContext}
+          appContext={appContext}
+          idx={idx}
+          key={message.message_id + "-" + idx}
+        />
+      )}
+      defaultAppSettings={{volume: 0}}
+      renderSidePane={({ appContext, mephistoContext: { taskConfig } }) => (
+        <DefaultTaskDescription
+          chatTitle={taskConfig.chat_title}
+          taskDescriptionHtml={taskConfig.task_description}
+        >
+          <b>You've been assigned: </b>
+          {appContext.taskContext?.persona?.name}
+          <p>
+            <b>Your Persona: </b>
+            {appContext.taskContext?.persona?.persona}
+          </p>
+          <b>You are in: </b>
+          {appContext.taskContext?.location?.name}
+          <p>
+            {appContext.taskContext?.location?.description}
+          </p>
+        </DefaultTaskDescription>
+      )}
+    />
+  );
+}
+
+ReactDOM.render(<MainApp />, document.getElementById("app"));
