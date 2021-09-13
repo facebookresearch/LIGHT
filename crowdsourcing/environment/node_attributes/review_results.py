20681a840d04c51c4ee44cd4b25e030248369c8b
Jack Urbanek <jju@fb.com>
2021-09-13 16:19:22 -0700

    Adding review script

--- crowdsourcing/environment/node_attributes/review_results.py
+++ crowdsourcing/environment/node_attributes/review_results.py
@@ -12,7 +12,53 @@ db = LocalMephistoDB()
 
 
 def format_data_for_printing(data):
-    worker_name = Worker(db, data["worker_id"]).worker_name
+    nodes = data['data']['inputs']
+    results = data['data']['outputs']['final_data']
+
+    node_type = nodes['itemCategory']
+    nodes = nodes['selection']
+    node_map = {node['name']: 
+        {
+            'desc': node['description'], 
+            'attributes': [a['name'] for a in node['attributes'] if a['value']],
+        } for node in nodes
+    }
+
+    display_nodes = [
+        f"{name}: {v['desc']}\n - attributes: [{', '.join(v['attributes'])}]"
+        for name, v in node_map.items()
+    ]
+    print("\n".join(display_nodes))
+
+    result_map = {n['name']: n['values']['custom'] + [k for k in n['values'].keys() if n['values'][k] and k != 'custom'] for n in results['nodes']}
+
+    print(result_map)
+
+    scale_attributes = results['attributes']
+    custom_scale_attributes = scale_attributes['custom_attributes']
+    del scale_attributes['custom_attributes']
+    display_attributes = []
+    for att_name, att_dict in scale_attributes.items():
+        items = [(k, v) for k, v in att_dict.items()]
+        if len(items) == 0:
+            continue
+        items.sort(key=lambda x: x[1])
+        item_names = '\t'.join([i[0] for i in items])
+        item_vals = '\t'.join([f"{i[1]:.3f}" for i in items])
+        display_attributes.append(
+            f"{att_name}:\n {item_names}\n{item_vals}"
+        )
+    for att_dict in custom_scale_attributes:
+        items = [(k, v) for k, v in att_dict['vals'].items()]
+        items.sort(key=lambda x: x[1])
+        item_names = '\t'.join([i[0] for i in items])
+        item_vals = '\t'.join([f"{i[1]:.3f}" for i in items])
+        display_attributes.append(
+            f"{att_dict['name']}: {att_dict['description']}\n {item_names}\n{item_vals}"
+        )
+    print("\n".join(display_attributes))
+
+    worker_name = Worker.get(db, data["worker_id"]).worker_name
     contents = data["data"]
     duration = contents["times"]["task_end"] - contents["times"]["task_start"]
     metadata_string = (
