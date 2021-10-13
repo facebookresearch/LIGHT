1be4c193a4b4711b6d6ffac178df9e589274f786
Jack Urbanek <jju@fb.com>
2021-10-13 11:03:02 -0700

    formatting

--- crowdsourcing/dialogues/multi_party_chat/run_task.py
+++ crowdsourcing/dialogues/multi_party_chat/run_task.py
@@ -33,6 +33,7 @@ hydra_defaults = [
 
 from mephisto.operations.hydra_config import RunScriptConfig, register_script_config
 
+
 @dataclass
 class ScriptConfig(RunScriptConfig):
     defaults: List[Any] = field(default_factory=lambda: hydra_defaults)
@@ -57,38 +58,41 @@ register_script_config(name="scriptconfig", module=ScriptConfig)
 
 
 def make_unit_validator(db, block_qual):
-    MIN_AVG_MESSAGE_LENGTH = 7 # 7 words should be easily achievable
-    SPAM_MESSAGE_TIMEOUT = 3 # Responding within 3 seconds of own message is likely spam
-    SPAM_MESSAGE_FORGIVENESS = 0.25 # If less than a quarter are spam, we don't penalize
-    TOO_LONG_TIME = 90 # Taking 2 minutes own messages is getting a bit long
-    LONG_MESSAGE_FORGIVENESS = 0.40 # So long as less than 40% are too long, we forgive
-    MAX_REPEAT_MESSAGE_PROPORTION = 0.30 # most messages should respond to others
+    MIN_AVG_MESSAGE_LENGTH = 7  # 7 words should be easily achievable
+    SPAM_MESSAGE_TIMEOUT = (
+        3  # Responding within 3 seconds of own message is likely spam
+    )
+    SPAM_MESSAGE_FORGIVENESS = (
+        0.25  # If less than a quarter are spam, we don't penalize
+    )
+    TOO_LONG_TIME = 90  # Taking 2 minutes own messages is getting a bit long
+    LONG_MESSAGE_FORGIVENESS = 0.40  # So long as less than 40% are too long, we forgive
+    MAX_REPEAT_MESSAGE_PROPORTION = 0.30  # most messages should respond to others
 
-     # Heuristic functions - return true if there's a possible red flag
+    # Heuristic functions - return true if there's a possible red flag
     # for the given worker
     def too_long_between_messages(m_info):
-        time_exceeds_cap = [m['d_last_self'] > TOO_LONG_TIME for m in m_info]
+        time_exceeds_cap = [m["d_last_self"] > TOO_LONG_TIME for m in m_info]
         exceed_cap_count = len([x for x in time_exceeds_cap if x])
         exceed_cap_prop = exceed_cap_count / len(m_info)
         return exceed_cap_prop > LONG_MESSAGE_FORGIVENESS
-        
+
     def too_short_between_messages(m_info):
-        time_below_min = [m['d_last_self'] < SPAM_MESSAGE_TIMEOUT for m in m_info]
+        time_below_min = [m["d_last_self"] < SPAM_MESSAGE_TIMEOUT for m in m_info]
         below_min_count = len([x for x in time_below_min if x])
         below_min_prop = below_min_count / len(m_info)
         return below_min_prop > SPAM_MESSAGE_FORGIVENESS
 
     def messages_too_short(m_info):
         """Check to find out the average message lengths"""
-        message_lengths = [len(m['text'].strip().split()) for m in m_info]
+        message_lengths = [len(m["text"].strip().split()) for m in m_info]
         avg_len = sum(message_lengths) / len(message_lengths)
         return avg_len < MIN_AVG_MESSAGE_LENGTH
 
     def too_many_repeated(m_info):
-        repeat_count = len([m for m in m_info if m['last_is_self']])
+        repeat_count = len([m for m in m_info if m["last_is_self"]])
         repeat_prop = repeat_count / len(m_info)
         return repeat_prop > MAX_REPEAT_MESSAGE_PROPORTION
-
 
     mephisto_data_browser = MephistoDataBrowser(db=db)
 
@@ -101,24 +105,30 @@ def make_unit_validator(db, block_qual):
         if output is None:
             return
 
-        messages = output['messages']
-        agent_name = output['agent_name']
+        messages = output["messages"]
+        agent_name = output["agent_name"]
 
         message_info = []
         for idx in range(len(messages)):
             m = messages[idx]
-            if m['id'] == agent_name:
-                last_timestamp = messages[idx-1]['timestamp']
-                last_self_timestamp = last_timestamp if len(message_info) == 0 else message_info[-1]['timestamp']
-                last_is_self = messages[idx-1]['id'] == agent_name
-                timestamp = m['timestamp']
-                message_info.append({
-                    'timestamp': timestamp,
-                    'd_last_self': timestamp - last_self_timestamp,
-                    'd_last_message': timestamp - last_timestamp,
-                    'last_is_self': last_is_self,
-                    'text': m['text'],
-                })
+            if m["id"] == agent_name:
+                last_timestamp = messages[idx - 1]["timestamp"]
+                last_self_timestamp = (
+                    last_timestamp
+                    if len(message_info) == 0
+                    else message_info[-1]["timestamp"]
+                )
+                last_is_self = messages[idx - 1]["id"] == agent_name
+                timestamp = m["timestamp"]
+                message_info.append(
+                    {
+                        "timestamp": timestamp,
+                        "d_last_self": timestamp - last_self_timestamp,
+                        "d_last_message": timestamp - last_timestamp,
+                        "last_is_self": last_is_self,
+                        "text": m["text"],
+                    }
+                )
 
         valids = [
             too_long_between_messages(message_info),
@@ -164,7 +174,11 @@ def main(cfg: DictConfig) -> None:
         world_opt["send_task_data"] = True
 
     shared_state = SharedParlAITaskState(
-        world_opt=world_opt, onboarding_world_opt=world_opt, on_unit_submitted=make_unit_validator(db, cfg.mephisto.blueprint.block_qualification)
+        world_opt=world_opt,
+        onboarding_world_opt=world_opt,
+        on_unit_submitted=make_unit_validator(
+            db, cfg.mephisto.blueprint.block_qualification
+        ),
     )
 
     if cfg.qualify_new_workers:
