412ff10d643ce07b82176e5190d6a7bf06eb2cd1
Justin Pinero <jpinero@fb.com>
2022-04-11 13:36:35 -0400

    added PreviewView, added assets to preview view, began styling preview view

--- crowdsourcing/light_gameplay/webapp/src/Views/PreviewView/index.js
+++ crowdsourcing/light_gameplay/webapp/src/Views/PreviewView/index.js
@@ -3,59 +3,31 @@ import React from "react";
 /* STYLES */
 import "./styles.css";
 /* IMAGES */
-import Scribe from "../../../assets/images/scribe.png";
-import ActionText1 from "../../../assets/screenshots/Tutorial/Interacting/Emote1.png";
-import WrappedText from "../../../assets/screenshots/Tutorial/Interacting/InteractingRespond.png";
+// CHAT
+import ChatBox from "../../assets/screenshots/chatbox.png";
+import DoChatBar from "../../assets/screenshots/dochatbar.png";
+import SayChatBar from "../../assets/screenshots/saychatbar.png";
+import TellChatBar from "../../assets/screenshots/tellchatbar.png";
+// HUD INFO
+import CharacterInfo from "../../assets/screenshots/characterinfo.png";
+import MissionInfo from "../../assets/screenshots/missioninfo.png";
+import SettingInfo from "../../assets/screenshots/settinginfo.png";
 
-//InstructionModalContent - renders content and copy for introduction modal that appears only to new players
-const InstructionModalContent = ({ buttonFunction }) => {
+
+//PreviewView - Renders orientation info and preview of task
+const PreviewView = ({
+
+ }) => {
   return (
-    <div className="instructionmodalcontent-container">
-      <div className="instructionmodalcontent-header">
-        <img className="instructionmodalcontent-scribe" src={Scribe} alt="" />
-        <h1 className="instructionmodalcontent-header__text">
-          Playing{" "}
-          <span className="instructionmodalcontent-header__light">LIGHT</span>
-        </h1>
+    <div className="previewview-container">
+      <div className="previewview-header">
+
       </div>
-      <div className="instructionmodalcontent-body">
-        <p className="instructionmodalcontent-body__text">
-          The LIGHT interface attempts to interpret anything typed into it as an
-          action.
-        </p>
-        <img
-          className="instructionmodalcontent-image"
-          src={ActionText1}
-          alt="example of chat input"
-        />
-        <p className="instructionmodalcontent-body__text">
-          To speak, you should wrap your sentence in quotes, or use shift+enter
-          to send your messages.
-        </p>
-        <img
-          className="instructionmodalcontent-image"
-          src={WrappedText}
-          alt="example of sentence wrapped in quotes"
-        />
-        <p className="instructionmodalcontent-body__text">
-          A list of commands can be displayed with "help" and you can find a
-          more complete list of instructions{" "}
-          <a style={{ color: "white" }} href={"/#/tutorial"}>
-            here
-          </a>
-          .
-        </p>
-      </div>
-      <div className="instructionmodalcontent-row">
-        <div
-          className="instructionmodalcontent-button"
-          onClick={buttonFunction}
-        >
-          READY
-        </div>
+      <div className="previewview-body">
+
       </div>
     </div>
   );
 };
 
-export default InstructionModalContent;
+export default PreviewView;
