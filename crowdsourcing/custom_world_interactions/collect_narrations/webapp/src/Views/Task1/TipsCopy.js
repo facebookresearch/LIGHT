a0d4e917a05c926de03c2d1a0c039140fed7b86a
Jack Urbanek <jju@fb.com>
2022-08-31 14:03:01 -0400

    Adding copyright headers

--- crowdsourcing/custom_world_interactions/collect_narrations/webapp/src/Views/Task1/TipsCopy.js
+++ crowdsourcing/custom_world_interactions/collect_narrations/webapp/src/Views/Task1/TipsCopy.js
@@ -1,3 +1,10 @@
+
+/*****
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const TipsCopy = [
     "Do not add traits to the objects no specifically mentioned in the description.",
     "Do not add items to the interaction other than the two specifically selected.",
--- crowdsourcing/environment/node_attributes/webapp/src/utils/GetFlagDimensions.js
+++ crowdsourcing/environment/node_attributes/webapp/src/utils/GetFlagDimensions.js
@@ -1,3 +1,10 @@
+
+/*****
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 //GetFlagDimensions - Function that returns an object  with flag styling dimensions and label based on the length of the label text
 const GetFlagDimensions = (
     label, //Flag Text
