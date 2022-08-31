53cce37bbc70edc64268bd38a6e1284055c6a4dc
Jack Urbanek <jju@fb.com>
2022-08-31 14:18:12 -0400

    a few more copyrights

--- light/graph/tests/test_nodes.py
+++ light/graph/tests/test_nodes.py
@@ -4,10 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Copyright (c) Facebook, Inc. and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.abs
-
 import unittest
 
 from light.graph.elements.graph_nodes import (
