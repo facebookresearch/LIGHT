d920fb9be0b1af163fc144225a2b86f42c1c01f4
Justin Pinero <jpinero@fb.com>
2022-09-09 07:11:19 -0400

    fixed modal opacity

--- deploy/web/gameapp/src/components/Modals/ReportMessageModal/index.js
+++ deploy/web/gameapp/src/components/Modals/ReportMessageModal/index.js
@@ -124,7 +124,7 @@ const ReportMessageForm = () => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative bg-gray-700 bg-opacity-90 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+              <Dialog.Panel className="relative bg-gray-700 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
                 <Dialog.Title
                   as="h3"
                   className="text-lg leading-6 font-medium underline text-white text-center"
