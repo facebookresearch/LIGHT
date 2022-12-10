2118c2d9950438af7bab1a60a7f64be9c1931cfc
JackUrb <jju@fb.com>
2022-12-10 00:13:09 +0000

    wrong form for adding model safety

--- light/world/souls/models/generative_heuristic_model_soul.py
+++ light/world/souls/models/generative_heuristic_model_soul.py
@@ -319,7 +319,7 @@ class GenerativeHeuristicModelSoul(OnEventSoul):
         act_text = self.dialogue_pick_non_repeating_response(act, partner)
 
         is_safe = await self.world.safety_classifier.is_safe(act_text)
-        reply_event = TellEvent(agent, target_nodes=[partner], text_content=act_text, is_safe=is_safe)
+        reply_event = TellEvent(agent, target_nodes=[partner], text_content=act_text, safe=is_safe)
         reply_event.execute(self.world)
 
     async def _take_timestep(self) -> None:
